91e18aeabe0fb43fb67edb9c812bc456dfa345d7
Dakrfox <business.nimh@outlook.com>
2023-05-14 10:29:04 -0500

    adding header effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,68 @@ import nuclear from "@icons/nuclear.svg"
 import Image from "next/image"
 import Footer from "@components/Footer"
 import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from "react"
 
 
 export default function Home() {
+  const elementRef = useRef(null);
   const router = useRouter();
+  const [headerColor, setHeaderColor] = useState(true);
+
+  const [appHeight, setAppHeight] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setAppHeight(window.innerHeight);
+    };
+
+    if (typeof window !== 'undefined') {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const elemento = elementRef.current;
+    const handleScroll = () => {
+      const posicionScroll = elemento.scrollTop;
+      if (posicionScroll < appHeight) {
+        setHeaderColor(true);
+      } else {
+        setHeaderColor(false);
+      }
+    };
+
+    elemento.addEventListener('scroll', handleScroll);
+
+    return () => {
+      elemento.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
 
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
+    <div className={styles.wrapper} ref={elementRef}>
+      <div id="header" className={`${styles.header} ${(headerColor) ? styles.color_white : styles.color_black}`}>
         <div className={styles.header_left}>
           <h2>DKF-MUSIC-PL</h2>
         </div>
         <div className={styles.header_right}>
           <ul className={styles.header_list}>
-            <li className={styles.header_list_item}><a href="#">About</a></li>
-            <li className={styles.header_list_item}><a href="#">Contact</a></li>
-            <li className={styles.header_list_item}><a href="#">DK-Fox</a></li>
+            <li className={styles.header_list_item}><a href="#hero" className={(headerColor) ? styles.color_white : styles.color_black} >Home</a></li>
+            <li className={styles.header_list_item}><a href="#about" className={(headerColor) ? styles.color_white : styles.color_black} >About</a></li>
+            <li className={styles.header_list_item}><a href="#contact" className={(headerColor) ? styles.color_white : styles.color_black}>Contact</a></li>
+            <li className={styles.header_list_item}><a href="https://dkfox.com.co" target="_blank" className={(headerColor) ? styles.color_white : styles.color_black}>DK-Fox</a></li>
           </ul>
         </div>
       </div>
       <section id="hero" className={styles.hero_container}>
-        
-        <div className={styles.rock_container} onClick={()=>router.push('/rock')}>
+
+        <div className={styles.rock_container} onClick={() => router.push('/rock')}>
           <div className={styles.content}>
             <div className={styles.icon}>
               <Image src={nuclear} alt="flame" />
@@ -37,8 +77,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        
-        <div className={styles.pop_container} onClick={()=>router.push('/pop')}>
+
+        <div className={styles.pop_container} onClick={() => router.push('/pop')}>
           <div className={styles.content}>
             <div className={styles.icon}>
               <Image src={moon} alt="flame" />
@@ -48,7 +88,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={styles.electro_container} onClick={()=>router.push('/electro')}>
+        <div className={styles.electro_container} onClick={() => router.push('/electro')}>
           <div className={styles.content}>
             <div className={styles.icon}>
               <Image src={prism} alt="flame" />
@@ -58,7 +98,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={styles.rap_container} onClick={()=>router.push('/rap')}>
+        <div className={styles.rap_container} onClick={() => router.push('/rap')}>
           <div className={styles.content}>
             <div className={styles.icon}>
               <Image src={flame} alt="flame" />
@@ -94,7 +134,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <Footer />
+      <Footer id="contact" />
     </div>
 
   );
