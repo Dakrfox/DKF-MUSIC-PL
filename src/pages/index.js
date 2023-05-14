c438bed7dd0fd499697ab26dc77befc2bb986df9
DKFOX <business.nimh@outlook.com>
2023-05-14 17:06:36 -0500

    adding control anvigation [inProgress]

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
       elemento.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
+console.log(headerColor)
   return (
 
     <div className={styles.wrapper} ref={elementRef}>
--- src/pages/rock/index.js
+++ src/pages/rock/index.js
@@ -1,10 +1,20 @@
 import styles from "@styles/Rock.module.scss"
+import Image from "next/image";
+import play_back from "@icons/play-back-outline.svg";
+import play_forward from "@icons/play-forward-outline.svg";
+import play_skip_back from "@icons/play-skip-back-outline.svg";
+import play_skip_forward from "@icons/play-skip-forward-outline.svg";
+
 
 export default function Rock() {
   return (
     <div className={styles.playlist_container}>
       <div className={styles.title}>
+        <Image className={styles.image} src={play_skip_back} width={50} height={50}/>
+        <Image className={styles.image} src={play_back} width={50} height={50}/>
         <h1>ROCK</h1>
+        <Image className={styles.image} styles="fill:white" src={play_forward}  width={50} height={50}/>
+        <Image className={styles.image} src={play_skip_forward} width={50} height={50}/>
       </div>
       <div className={styles.playlist}>
         <ul className={styles.list}>
